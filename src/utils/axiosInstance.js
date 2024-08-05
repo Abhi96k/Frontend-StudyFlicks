397de2fa3d634747f892e52ef9190da6e0a3feae
Abhishek Nangare <88939013+Abhi96k@users.noreply.github.com>
2024-08-05 22:05:52 +0530

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,13 +1,33 @@
+// import axios from "axios";
 
-import axios from 'axios';
+// const axiosInstance = axios.create({
+//   baseURL: "http://localhost:8000/api/v1", // Your base URL here
+// });
+
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const accessToken = localStorage.getItem("accessToken");
+//     if (accessToken) {
+//       config.headers.Authorization = `Bearer ${accessToken}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default axiosInstance;
+
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://backend-studyflick.onrender.com', // Your base URL here
+  baseURL: "https://backend-studyflick.onrender.com/api/v1", // Updated base URL
 });
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const accessToken = localStorage.getItem('accessToken');
+    const accessToken = localStorage.getItem("accessToken");
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
