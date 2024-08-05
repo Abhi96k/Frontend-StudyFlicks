c2c27ddbcd8d446c780634520c5bf8d81b084e6a
Abhishek Nangare <88939013+Abhi96k@users.noreply.github.com>
2024-08-05 21:03:24 +0530

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api/v1', // Your base URL here
+  baseURL: 'https://backend-studyflick.onrender.com', // Your base URL here
 });
 
 axiosInstance.interceptors.request.use(
